184be0182a536c08ef86380458ef0e28e184eb98
Nathan Orlowski <nathan@Someone.local>
2024-12-04 17:06:44 +0200

    1

--- Primes.java
+++ Primes.java
@@ -1,5 +1,16 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);        
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false; 
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false; 
+            }
+        }
+        return true; 
     }
 }
