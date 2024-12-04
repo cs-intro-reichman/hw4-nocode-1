e4b521bdc0bf7bcb07b46e88cdff0e721293da99
Nathan Orlowski <nathan@Someone.local>
2024-12-04 17:37:19 +0200

    2

--- Primes.java
+++ Primes.java
@@ -1,6 +1,23 @@
 public class Primes {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);        
+        // Convert the first argument (args[0]) to an integer
+        int n = Integer.parseInt(args[0]);    
+        int sum = 0;
+        int percentage = (sum/n) * 100;
+
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                sum +=1;
+                System.out.println(i);
+                
+
+            }
+            
+        }    
+
+        System.out.println("There are " + sum + " primes between 2 and " + n + " (" + percentage + "% are primes)");
+
     }
 
     public static boolean isPrime(int n) {
@@ -8,9 +25,9 @@ public class Primes {
 
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                return false; 
+                return false; // If divisible, n is not prime
             }
         }
-        return true; 
+        return true; // If no divisors are found, n is prime
     }
 }
