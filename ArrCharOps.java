498e7be1eb3a5f4585bedddf595c757e25b12751
Nathan Orlowski <nathan@Someone.local>
2024-12-05 11:09:54 +0200

    2

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -59,17 +59,13 @@ public class ArrCharOps {
     /** Returns the index within the given array of the first occurrence of the given character.
      *  If no such character is found, returns -1.
      */
-   
-        public static int indexOf(char[] arr, char ch) {
-            if (arr[0] == ch) {
-                return 0;
-            }
-            for (int i = 1; i < arr.length; i++) {
+    public static int indexOf(char[] arr, char ch) {
+            for (int i = 0; i < arr.length; i++) {
                 if (arr[i] == ch) {
                     return i;
                 }
             }
-                        return ch;
+                        return -1;
         }
 
     /** Same as indexOf(char[], char), but starts the search in the given index.
